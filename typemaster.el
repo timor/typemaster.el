7c7e447143641c9e7afa6b385aee59768368aeda
timor <timor.dd@googlemail.com>
2018-04-07 00:20:16 +0200

    increase char penalties

--- typemaster.el
+++ typemaster.el
@@ -198,7 +198,7 @@
    ;; (message "Penalties: %s" (mapcar (lambda(x) (cons (string (car x)) (cdr x))) typemaster-prob-adjustments))
    else do (incf mismatches)
    ;; (message "increasing adjust for '%s'" (string test))
-   (incf (alist-get test typemaster-prob-adjustments 0) 2)
+   (incf (alist-get test typemaster-prob-adjustments 0) 3)
    (when last-read
      (let ((digram (string last-read test)))
        (unless (seq-contains digram 32)
